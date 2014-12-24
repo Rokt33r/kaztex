3f7826f57dfefa780a79a3b5311706ae595cbd2b
Dick Choi <fluke8259@gmail.com>
2014-12-24 19:05:55 +0900

    missing to commit

--- app/controllers/apis/UserFilesController.php
+++ app/controllers/apis/UserFilesController.php
@@ -20,15 +20,15 @@ class UserFilesController extends BaseController{
             return Response::make(['file'=>null], 400);
         }
 
-        if(Input::get('order')==='load'){
-            $data = Flysystem::read($path);
-            return ['data'=>$data];
-        }
-
         $file = Flysystem::get($path);
 
         $type = $file->getType();
         $path = $file->getPath();
+
+        if($type === 'file' && Input::get('order')==='load'){
+            $data = Flysystem::read($path);
+            return ['data'=>$data];
+        }
 
         if($type === 'file'){
             $size = $file->getSize();
